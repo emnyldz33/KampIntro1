33f39b8e262e7aabcf7ee714423662261c6799af
Emin <emnyldz33@gmail.com>
2021-01-26 23:10:36 +0300

    4.Ders Sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Emin", "Murat", "Veli", "Serkan" };
+            //Console.WriteLine(0);
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);
+            //Console.WriteLine(3);
+
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(4);
+            //Console.WriteLine(0);
+
+            List<string> isimler2 = new List<string> { "Emin", "Murat", "Veli", "Serkan" };
+            
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
