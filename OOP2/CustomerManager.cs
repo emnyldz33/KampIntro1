577486fd0bec69ad92771bcd6593266834a99cb5
Emin <emnyldz33@gmail.com>
2021-02-02 23:27:26 +0300

    5.gün 2.mola

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class CustomerManager
+    {   //İş Sınıfları //Business Classes
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Emin YILDIZ
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Emin";
+            musteri1.Soyadi = "Yıldız";
+            musteri1.TcNo = "11111111111";
+
+            //Astor
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Astor";
+            musteri2.VergiNo = "123456789";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+
+        }
+    }
+}
